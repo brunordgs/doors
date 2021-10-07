a615401621e1ac80790e7f434e6d851c161e6020
Bruno Rodrigues <brunohrsilva1@gmail.com>
2021-10-07 08:10:50 -0300

    Add gift styles

--- src/components/Gift.tsx
+++ src/components/Gift.tsx
@@ -1,8 +1,10 @@
 export default function Gift() {
 	return (
-		<div className="flex">
-			<div className="w-24 h-6 bg-[#bd93f9]">Tampa</div>
-			<div className="">corpo</div>
+		<div className="flex flex-col items-center">
+			<div className="w-[100px] h-[25px] bg-[#7aa944]"></div>
+			<div className="w-[90px] h-[60px] bg-[#5c7e32]"></div>
+			<div className="absolute bg-red-600 w-[15px] h-[85px]"></div>
+			<div className="absolute bg-red-600 w-[90px] h-[15px] top-[40px]"></div>
 		</div>
 	);
 }

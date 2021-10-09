25ba2a6c54d698f810ad20e161d34614bb01334d
Bruno Rodrigues <brunohrsilva1@gmail.com>
2021-10-09 19:18:24 -0300

    Add input values validation

--- src/pages/game/[doors]/[hasGift].tsx
+++ src/pages/game/[doors]/[hasGift].tsx
@@ -8,6 +8,15 @@ export default function Game() {
 	const router = useRouter();
 
 	const [doors, setDoors] = useState([]);
+	const [isValid, setIsValid] = useState(false);
+
+	useEffect(() => {
+		const { doors, hasGift } = router.query;
+		const validDoorsAmount = +doors >= 3 && +doors <= 100;
+		const validGiftInput = +hasGift >= 1 && +hasGift <= +doors;
+
+		setIsValid(validDoorsAmount && validGiftInput);
+	}, [doors]);
 
 	useEffect(() => {
 		const { doors, hasGift } = router.query;
@@ -31,7 +40,13 @@ export default function Game() {
 
 	return (
 		<div className="flex flex-col items-center justify-center h-screen">
-			<div className="flex flex-wrap self-stretch justify-around">{renderDoors()}</div>
+			<div className="flex flex-wrap self-stretch justify-around">
+				{isValid ? (
+					renderDoors()
+				) : (
+					<h1 className="text-4xl font-bold">Please, select a valid amount :(</h1>
+				)}
+			</div>
 
 			<div className="flex justiy-center mt-[40px] bg-red-500 hover:bg-red-600 text-white text-[2rem] rounded-md cursor-pointer">
 				<Link href="/">
